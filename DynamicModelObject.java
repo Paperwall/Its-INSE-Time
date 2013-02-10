1f9ff4c1ee6b331698d4a64561d92e8ea1e6b465
Frazer S <up629547@myport.ac.uk>
2013-02-10 14:52:18 +0000

    Table cells made editable, changes to methods
    changes made to methods to reflect the Task object modifications.

--- DynamicModelObject.java
+++ DynamicModelObject.java
@@ -3,6 +3,7 @@ package start;
 
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
@@ -13,7 +14,7 @@ public class DynamicModelObject extends AbstractTableModel {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private final List tasks = new ArrayList();
+	private final List<Task> tasks = new ArrayList<Task>();
 	
 	private final String[] columnHeaders = {"Task",
 			"Task No.",
@@ -24,10 +25,7 @@ public class DynamicModelObject extends AbstractTableModel {
 	
 	public DynamicModelObject() {
 		super();
-		
-		tasks.add(new Task("","", null, null, null, null));
-		tasks.add(new Task("","", null, null, null, null));
-		tasks.add(new Task("","", null, null, null, null));
+
 	}
 
 	public int getColumnCount() {
@@ -45,19 +43,62 @@ public class DynamicModelObject extends AbstractTableModel {
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		switch(columnIndex){
 		case 0:
-			return tasks.get(rowIndex).getTaskName();
+			return ((Task) tasks.get(rowIndex)).getTaskName();
 		case 1:
-			return tasks.get(rowIndex).getTaskNo();
+			return ((Task) tasks.get(rowIndex)).getTaskNo();
 		case 2:
-			return tasks.get(rowIndex).getStartDate();
+			return ((Task) tasks.get(rowIndex)).getStartDate();
 		case 3:
-			return tasks.get(rowIndex).getFinishDate();
+			return ((Task) tasks.get(rowIndex)).getFinishDate();
 		case 4:
-			return tasks.get(rowIndex).getActualStartDate();
+			return ((Task) tasks.get(rowIndex)).getActualStartDate();
 		case 5:
-			return tasks.get(rowIndex).getActualFinishDate();
+			return ((Task) tasks.get(rowIndex)).getActualFinishDate();
 		default:
 			return null;
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Class getColumnClass(int columnIndex) {
+		switch(columnIndex){
+		case 2:
+			return String.class; 
+		case 3:
+			return Date.class;
+		default:
+			return Object.class;
+		}
+	}
+	
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return true;
+	}
+	
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		if(aValue != null){
+			Task task = tasks.get(rowIndex);
+			
+			switch(columnIndex){
+			    case 0:
+			    	task.setTaskName((String)aValue);
+			    	break;
+			    case 1:
+			    	task.setTaskNo((String)aValue);
+			    	break;
+			    case 2:
+			    	task.setStartDate((String)aValue);
+			    	break;
+			    case 3:
+			    	task.setFinishDate((String)aValue);
+			    	break;
+			    case 4:
+			    	task.setActualStartDate((String)aValue);
+			    	break;
+			    case 5:
+			    	task.setActualFinishDate((String)aValue);
+			    	break;
+			}
 		}
 	}
 	
@@ -67,7 +108,7 @@ public class DynamicModelObject extends AbstractTableModel {
 		fireTableRowsInserted(tasks.size() -1, tasks.size() -1);
 	}
 	
-	public void removeFriend(int rowIndex) {
+	public void removeTask(int rowIndex) {
 		tasks.remove(rowIndex);
 		
 		fireTableRowsDeleted(rowIndex, rowIndex);
