b8c31ce3e06a3091420cb4250d1b45ba260a7ca7
Gary <up616651@myport.ac.uk>
2013-02-15 14:17:05 +0000

    attempt at traversing table in wbt.java

--- WorkBreakdownTree.java
+++ WorkBreakdownTree.java
@@ -2,14 +2,13 @@ package start;
 
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.text.StyledDocument;
 
 public class WorkBreakdownTree extends JFrame{  
 	
 
 	private static final long serialVersionUID = 1L;
 
-	public WorkBreakdownTree() {
+	public WorkBreakdownTree() {		
 		super();
 		setType(Type.POPUP);
 		setTitle("Work Breakdown Tree");
@@ -19,11 +18,43 @@ public class WorkBreakdownTree extends JFrame{
 		setPreferredSize(new Dimension(400, 300));
 		getContentPane().setBackground(SystemColor.desktop);
 		getContentPane().setLayout(null); 
-		
 		JTextPane wbtContent = new JTextPane();
-		wbtContent.setText("some text");
+		for (int i = 0; i < InputInterface.model.getRowCount(); i++){
+		      String value = (String) InputInterface.model.getValueAt(i, 1);
+		      wbtContent.setText(value);
+		      System.out.print("row traversed!"); //debug code
+		}
 		wbtContent.setBounds(0, 0, 250, 400);
 		getContentPane().add(wbtContent);
-		
 	}
 }
+
+/*
+ * 		super();
+		setType(Type.POPUP);
+		setTitle("Work Breakdown Tree");
+		
+		setResizable(false);
+		setSize(new Dimension(360, 462));
+		setPreferredSize(new Dimension(400, 300));
+		getContentPane().setBackground(SystemColor.desktop);
+		getContentPane().setLayout(null); 
+		
+		 TablePrintDemo newContentPane = new TablePrintDemo();
+	     newContentPane.setOpaque(true); //content panes must be opaque
+	     setContentPane(newContentPane);
+		//JTextPane wbtContent = new JTextPane();
+		InputInterface.model.print();
+		//wbtContent.setBounds(0, 0, 250, 400);
+		 * 
+		 * 
+		 * 
+		 * super();
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane();
+
+        //Add the scroll pane to this panel.
+        add(scrollPane);
+		 */
