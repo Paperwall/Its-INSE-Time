7c043e7cc1711f7f02d81571d54671f3cd98b863
Gary <up616651@myport.ac.uk>
2013-02-15 11:20:15 +0000

    WBTClass 1.1
    fixed work breakdown tree panel, now draws. needs actual functionality

--- WorkBreakdownTree.java
+++ WorkBreakdownTree.java
@@ -1,28 +1,28 @@
 package start;
 
 import java.awt.*;
+import java.awt.Window.Type;
+
 import javax.swing.*;
 
-public class WorkBreakdownTree {
+public class WorkBreakdownTree extends JFrame{  
 	
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public WorkBreakdownTree() {
 		super();
+		setType(Type.POPUP);
+		setTitle("Work Breakdown Tree");
 		
-		JFrame wbtFrame = new JFrame("HelloWorldSwing");
-        wbtFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        
-        wbtFrame.setResizable(false);
-		wbtFrame.setSize(new Dimension(800, 600));
-		wbtFrame.setPreferredSize(new Dimension(800, 600));
-
-
-        JLabel label = new JLabel("Hello World");
-        wbtFrame.getContentPane().add(label);
-
-        //Display the window.
-        wbtFrame.pack();
-		
+		setResizable(false);
+		setSize(new Dimension(360, 462));
+		setPreferredSize(new Dimension(400, 300));
+		getContentPane().setBackground(SystemColor.desktop);
+		getContentPane().setLayout(null); 
+		JLabel lblNewLabel = new JLabel("THIS IS WBT LOL");
+		lblNewLabel.setBounds(0, 0, 356, 438);
+		getContentPane().add(lblNewLabel);
 	}
+
 }
